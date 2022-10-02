4552689d4fbcb1a606725c8b2292f61f2a1cc2b3
MADHU SHUBHAM MAHORE <57442376+madhushubhammahore@users.noreply.github.com>
2022-10-02 12:41:03 +0530

    Add files via upload

---
+++ recursive parsing.c
@@ -0,0 +1,44 @@
+int main()
+{
+    // E is a start symbol.
+    E();
+ 
+    // if lookahead = $, it represents the end of the string
+    // Here l is lookahead.
+    if (l == '$')
+        printf("Parsing Successful");
+}
+ 
+// Definition of E, as per the given production
+E()
+{
+    if (l == 'i') {
+        match('i');
+        E'();
+    }
+}
+ 
+// Definition of E' as per the given production
+E'()  
+{
+    if (l == '+') {
+        match('+');
+        match('i');
+        E'();
+    }//The second condition of E'
+    else if ( l == 'e' )
+    {
+      match('e');
+    }
+        return ();
+}
+ 
+// Match function
+match(char t)
+{
+    if (l == t) {
+        l = getchar();
+    }
+    else
+        printf("Error");
+}
